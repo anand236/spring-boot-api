45c263cfd2d74c55d4ad95f45a12464189ab9a86
anand236 <choudhary23.c2@gmail.com>
2026-05-05 23:28:05 +0530

    securityConfig overriding cors option

--- src/main/java/create_api/test/config/SecurityConfig.java
+++ src/main/java/create_api/test/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package create_api.test.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -16,6 +17,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                HttpMethod.OPTIONS, "/**"
+                        ).permitAll()
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
